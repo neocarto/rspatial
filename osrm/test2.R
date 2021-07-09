0319fd8208b097f9202e0f65218ecda1ab84bb9f
Nicolas Lambert <nicolas.lambert@cnrs.fr>
2021-07-09 18:31:41 +0200

    tmp all to paris

--- osrm/test2.R
+++ osrm/test2.R
@@ -6,16 +6,10 @@ lau2 = st_read("https://gisco-services.ec.europa.eu/distribution/v2/lau/geojson/
 
 # Data handling
 dots = st_centroid(lau2[lau2$CNTR_CODE == "FR",])
-
 dots = cbind(dots,st_coordinates(dots))
 dots = dots[,c("id","POP_2020","AREA_KM2","X","Y")] %>% st_drop_geometry()
 colnames(dots) = c("id","pop","area","lon","lat")
 dots = dots[dots$lat > 40.93,]
-
-
-
-
-write.csv(dots,"data/TimeDistToRoma.csv", row.names = FALSE )
 
 # Coordinates and ids
 coords = paste(paste(dots$lon,dots$lat,sep=","), collapse = ";")
@@ -25,16 +19,36 @@ ids = dots$id
 target = "FR_75056"
 destination <- match(target, dots$id) - 1
 
+getdist = function(start){
+
+# source
+end = start + 999
+if(end > dim(dots)[1]){end = dim(dots)[1]-1}
+source = paste(rep(start:end), collapse = ";")
+
 # Get a matrix
 router = "http://0.0.0.0:5000"
-query = paste0(router,"/table/v1/driving/",coords,"?destinations=",destination,"&annotations=duration")
+query = paste0(router,"/table/v1/driving/",coords,"?destinations=",destination,"&sources=",source,"&annotations=duration")
+start_time <- Sys.time()
 response = RCurl::getURL(query)
+end_time <- Sys.time()
+end_time - start_time
 
 # Data Handling
 output = rjson::fromJSON(response)
 output = data.frame(output$duration)
-output = cbind(id = ids, output)
+
+
+output = cbind(id = ids[(start+1):(end+1)], output)
 colnames(output) = c("id","duration")
 
 # Export data
-write.csv(output,"data/TimeDistToRoma.csv", row.names = FALSE )
+file = paste0("tmp/TimeDistToParis_",start,"_",end,".csv")
+write.csv(output,file, row.names = FALSE )
+}
+
+for(i in seq(from=0, to=34000, by=1000)){
+getdist(i)
+}
+
+
